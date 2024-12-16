325796daf8044f507292ddd5ddb26220a2735a0c
bukanfufufafa <nabilrizky249@gmail.com>
2024-12-16 16:03:44 +0700

    update 4

--- khusus_pelanggan/katalog.py
+++ khusus_pelanggan/katalog.py
@@ -2,12 +2,12 @@ import csv
 import sys
 import json
 
-def save_to_csv(pilihan, barang, alamat, username, name, status):
+def save_to_csv(pilihan, barang, kuantitas, total, tip, alamat, username, name, status):
     # Menyimpan data pesanan ke file CSV
     with open('pesanan.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         # Format: [pilihan, barang, alamat, username, name]
-        writer.writerow([pilihan, barang, alamat, username, name, status])
+        writer.writerow([pilihan, barang, kuantitas, total, tip, alamat, username, name, status])
     print(f"Pesanan {barang} telah disimpan ke file pesanan.csv.\n")
 
 def show_catalog(data_akun):
@@ -34,31 +34,40 @@ def beli_barang(data_akun):
     print("\nAnda memilih jasa titip 'Beli Barang'.")
     barang = input("Masukkan nama barang yang ingin dibeli: ").strip()
     alamat = input("Masukkan alamat pengiriman: ").strip()
-    
+    kuantitas = int(input("Masukkan jumlah barang: "))
+    harga = int(input("Harga barang satuan (Rupiah): "))
+    tip = int(input("Upah untuk mitra (Rupiah): ")) 
+    total = kuantitas * harga 
     print(f"\nPesanan Anda: {barang} akan dikirim ke {alamat}.")
     
     # Simpan pesanan ke CSV
-    save_to_csv("Beli Barang", barang, alamat, data_akun["username"], data_akun["name"], "dipesan")
+    save_to_csv("Beli Barang", barang, kuantitas, total, tip, alamat, data_akun["username"], data_akun["name"], "dipesan")
 
 def makanan(data_akun):
     print("\nAnda memilih jasa titip 'Makanan'.")
     makanan = input("Masukkan nama makanan yang ingin dibeli: ").strip()
     alamat = input("Masukkan alamat pengiriman: ").strip()
-    
+    kuantitas = int(input("Masukkan jumlah makanan: "))
+    harga = int(input("Harga makanan satuan (Rupiah): "))
+    tip = int(input("Upah untuk mitra (Rupiah): ")) 
+    total = kuantitas * harga 
     print(f"\nPesanan Anda: {makanan} akan dikirim ke {alamat}.")
     
     # Simpan pesanan ke CSV
-    save_to_csv("Makanan", makanan, alamat, data_akun["username"], data_akun["name"], "dipesan")
+    save_to_csv("Makanan", makanan, kuantitas, total, tip, alamat, data_akun["username"], data_akun["name"], "dipesan")
 
 def barang_tugas(data_akun):
     print("\nAnda memilih jasa titip 'Barang Tugas'.")
     tugas = input("Masukkan jenis barang tugas yang ingin dibeli: ").strip()
     alamat = input("Masukkan alamat pengiriman: ").strip()
-    
+    kuantitas = int(input("Masukkan jumlah barang: "))
+    harga = int(input("Harga barang satuan (Rupiah): "))
+    tip = int(input("Upah untuk mitra (Rupiah): ")) 
+    total = kuantitas * harga 
     print(f"\nPesanan Anda: {tugas} untuk tugas akan dikirim ke {alamat}.")
     
     # Simpan pesanan ke CSV
-    save_to_csv("Barang Tugas", tugas, alamat, data_akun["username"], data_akun["name"], "dipesan")
+    save_to_csv("Barang Tugas", tugas, kuantitas, total, tip, alamat, data_akun["username"], data_akun["name"], "dipesan")
 
 if __name__ == "__main__":
     # Menerima data akun dari argumen command line
